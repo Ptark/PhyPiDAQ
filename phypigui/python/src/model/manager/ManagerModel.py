d29327df2aee8a8c7b12d47639549e60a5ef3b43
Fritz Hund <edifice444@gmail.com>
2020-01-09 15:26:43 +0100

    Implement class ManagerModel

--- phypigui/python/src/model/manager/ManagerModel.py
+++ phypigui/python/src/model/manager/ManagerModel.py
@@ -1,39 +1,60 @@
+from typing import Dict, List, NoReturn
 
 class ManagerModel:
-    __sensors = List[SensorItem]
-    __diagrams = List[DiagramItem]
-    __sensor_data = Dict[SensorItem, ]
-    __running = False
-    selected_item = OutputItem
+    __sensors: List[SensorItem] = []
+    __diagrams: List[DiagramItem] = []
+    __sensor_data: Dict[SensorItem, []] = {}
+    __running: bool = False
+    selected_item: OutputItem = None
 
     @staticmethod
     def __init_functions() -> NoReturn:
+        for diagram in ManagerModel.__diagrams:
+            diagram.calculate_functions()
         return
 
     @staticmethod
-    def __read_data() -> bool:
+    def __read_data() -> NoReturn:
+        for sensor in ManagerModel.__sensor_data:
+            ManagerModel.__sensor_data[sensor] = sensor.read()
         return
+
+    @staticmethod
+    def is_running() -> bool:
+        return ManagerModel.__running
 
     @staticmethod
     def add_sensor(sensor: SensorItem) -> NoReturn:
+        ManagerModel.__sensors.append(sensor)
         return
 
     @staticmethod
     def delete_sensor(sensor: SensorItem) -> NoReturn:
+        ManagerModel.__sensors.remove(sensor)
         return
 
     @staticmethod
     def add_diagram(diagram: DiagramItem) -> NoReturn:
+        ManagerModel.__diagrams.append(diagram)
         return
 
     @staticmethod
     def delete_diagram(diagram: DiagramItem) -> NoReturn:
+        ManagerModel.__diagrams.remove(diagram)
         return
 
     @staticmethod
     def start() -> NoReturn:
+        ManagerModel.__init_functions()
+        ManagerModel.__running = True
+        while ManagerModel.__running:
+            ManagerModel.__read_data()
+            for diagram in ManagerModel.__diagrams:
+                diagram.calculate(ManagerModel.__sensor_data.copy())
+            
         return
 
     @staticmethod
     def stop() -> NoReturn:
+        ManagerModel.__running = False
         return
