90eacc669b78102d90d6ec44141f786a5fe5ef00
Fritz Hund <edifice444@gmail.com>
2020-01-23 23:58:25 +0100

    Implement class InputItem

---
+++ phypigui/python/src/model/item/InputItem.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from ..config import ConfigModel
+from . import ItemModel, Input
+from ..workspace import WorkspaceModel
+from typing import List, NoReturn
+
+
+class InputItem(ItemModel, ABC):
+    def __init__(self, name: str, description: str, config: ConfigModel.ConfigModel, inputs: int):
+        super().__init__(name, description, config)
+        self.__inputs: List[Input.Input] = []
+        for i in range(1, inputs):
+            self.__inputs.append(Input.Input(self.__id))
+
+    @abstractmethod
+    def get_number_of_inputs(self) -> int:
+        """Returns maximum number of inputs for this item
+
+        Returns:
+            int: Maximum number of inputs
+        """
+        pass
+
+    def connect_input(self, input_index: int, output_id: int) -> NoReturn:
+        """Connects an input with an output
+
+        Connects one input of this item to an output identified by its ID
+
+        Args:
+            input_index (int): Number of the input, which will be connected
+            output_id (int): ID of output, which is part of the connection
+        """
+        if 0 <= input_index <= self.get_number_of_inputs():
+            input_id: int = self.__inputs[input_index].id
+            if WorkspaceModel.WorkspaceModel.check_output_id(output_id) and +\
+                    WorkspaceModel.WorkspaceModel.check_input_id(input_id):
+                WorkspaceModel.WorkspaceModel.connect(input_id, output_id)
