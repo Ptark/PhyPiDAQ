f68b7a9e80f650ea66665266ecaf06feb61fac79
Fritz Hund <edifice444@gmail.com>
2020-01-27 08:54:15 +0100

    Add ConfigView module

---
+++ phypigui/python/src/view/config/BoolOptionView.py
@@ -0,0 +1,4 @@
+class BoolOptionView:
+
+    def __init__(self):
+        pass
---
+++ phypigui/python/src/view/config/ConfigView.py
@@ -0,0 +1,15 @@
+from python.src.model.config import ConfigModel
+from typing import List, NoReturn
+
+
+class ConfigView:
+
+    def __init__(self, config: ConfigModel.ConfigModel):
+        pass
+
+    def __init__window(self) -> NoReturn:
+        pass
+
+    def __on_close(self) -> NoReturn:
+        pass
+
