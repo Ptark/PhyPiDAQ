7cc6d5d3f0fcf46600a61fad3cbf927804a247d5
Fritz Hund <edifice444@gmail.com>
2020-02-02 00:34:43 +0100

    Add documentation for OperatorItem

--- phypigui/python/src/model/item/OperatorItem.py
+++ phypigui/python/src/model/item/OperatorItem.py
@@ -1,28 +1,46 @@
 from abc import ABC
-from ..item.InputItem import InputItem
+from typing import List, Callable, Dict
+
 from ..item.Output import Output
+from ..item.InputItem import InputItem
 from ..item.OutputItem import OutputItem
 from ..item.SensorItem import SensorItem
 from ..config.ConfigModel import ConfigModel
 from ..workspace.WorkspaceModel import WorkspaceModel
-from typing import List, Callable, Dict
 
 
 class OperatorItem(InputItem, OutputItem, ABC):
-    """Superclass for all kind of operators"""
+    """This class is a superclass for all kind of OperatorItems"""
+
     def __init__(self, name: str, description: str, config: ConfigModel, inputs: int, outputs: int):
+        """Initialising a OperatorItem object
+
+        Args:
+            name (str): Name of this OperatorItem
+            description (str): Description of this OperatorItem
+            config (ConfigModel): A configuration of adjustable options for this OperatorItem
+            inputs (int): Count of outputs for this OperatorItem
+            outputs (int): Count of outputs for this OperatorItem
+        """
         InputItem.__init__(self, name, description, config, inputs)
+
         self._outputs: List[Output] = []
         for i in range(1, outputs):
             self._outputs.append(Output(self._id, i))
 
 
 class DivisionOperatorItem(OperatorItem):
-    """Class models a division operator"""
+    """This class models a operator, which divides the first by the second data-stream
+
+    A DivisionOperatorItem has two inputs and one output.
+    """
+
     def __init__(self):
+        """Initialising a DivisionOperatorItem object"""
         name: str = "Divisionsoperator"
         description: str = "Dieser Operator dividiert zwei Werte"
         config: ConfigModel = ConfigModel()
+
         super().__init__(name, description, config, 2, 1)
 
     def get_rule(self, output_number: int = 0) -> Callable[[Dict[SensorItem, List[float]]], float]:
@@ -33,19 +51,22 @@ class DivisionOperatorItem(OperatorItem):
         return lambda data: first_function(data) / second_function(data)
 
     def get_unit(self, output_number: int) -> str:
-        """Returns the units from inputs and concatenate with division operator"""
-
         return "(" + WorkspaceModel.calculate_unit(self._inputs[0].id) + ") / (" +\
             WorkspaceModel.calculate_unit(self._inputs[1].id) + ")"
 
 
 class AbsoluteOperatorItem(OperatorItem):
-    """Class models an absolute operator"""
+    """This class models an operator, which makes a data-stream only positive
+
+    A AbsoluteOperatorItem has one inputs and one output.
+    """
 
     def __init__(self):
+        """Initialising a AbsoluteOperatorItem object"""
         name: str = "Absolutoperator"
         description: str = "Dieser Operator berechnet den Absolutbetrag"
         config: ConfigModel = ConfigModel()
+
         super().__init__(name, description, config, 1, 1)
 
     def get_rule(self, output_number: int = 0):
@@ -54,18 +75,21 @@ class AbsoluteOperatorItem(OperatorItem):
         return lambda data: abs(first_function(data))
 
     def get_unit(self, output_number: int) -> str:
-        """Returns the unit from input"""
-
         return "(" + WorkspaceModel.calculate_unit(self._inputs[0].id) + ")"
 
 
 class MultiplicationOperatorItem(OperatorItem):
-    """Class models a multiplication operator"""
+    """This class models an operator, which multiply two data-streams
+
+    A MultiplicationOperatorItem has two inputs and one output.
+    """
 
     def __init__(self):
+        """Initialising a MultiplicationOperatorItem object"""
         name: str = "Multiplikationsoperator"
         description: str = "Dieser Operator multipliziert zwei Werte"
         config: ConfigModel = ConfigModel()
+
         super().__init__(name, description, config, 2, 1)
 
     def get_rule(self, output_number: int = 0):
@@ -76,16 +100,18 @@ class MultiplicationOperatorItem(OperatorItem):
         return lambda data: first_function(data) * second_function(data)
 
     def get_unit(self, output_number: int) -> str:
-        """Returns the units from inputs and concatenate with multiplication operator"""
-
         return "(" + WorkspaceModel.calculate_unit(self._inputs[0].id) + ") * (" +\
             WorkspaceModel.calculate_unit(self._inputs[1].id) + ")"
 
 
 class AdditionOperatorItem(OperatorItem):
-    """Class models an addition operator"""
+    """This class models an operator, which adds two data-streams
+
+    A AdditionOperatorItem has two inputs and one output.
+    """
 
     def __init__(self):
+        """Initialising a AdditionOperatorItem object"""
         name: str = "Additionsoperator"
         description: str = "Dieser Operator addiert zwei Werte"
         config: ConfigModel = ConfigModel()
@@ -99,16 +125,18 @@ class AdditionOperatorItem(OperatorItem):
         return lambda data: first_function(data) + second_function(data)
 
     def get_unit(self, output_number: int) -> str:
-        """Returns the units from inputs and concatenate with addition operator"""
-
         return "(" + WorkspaceModel.calculate_unit(self._inputs[0].id) + ") + (" +\
             WorkspaceModel.calculate_unit(self._inputs[1].id) + ")"
 
 
 class SubtractionOperatorItem(OperatorItem):
-    """Class models a subtraction operator"""
+    """This class models an operator, which subtract the second of the first data-stream
 
-    def __init__(self) :
+    A SubtractionOperatorItem has two inputs and one output.
+    """
+
+    def __init__(self):
+        """Initialising a SubtractionOperatorItem object"""
         name: str = "Subtraktionsoperator"
         description: str = "Dieser Operator subtrahiert zwei Werte"
         config: ConfigModel = ConfigModel()
@@ -122,7 +150,5 @@ class SubtractionOperatorItem(OperatorItem):
         return lambda data: first_function(data) - second_function(data)
 
     def get_unit(self, output_number: int) -> str:
-        """Returns the units from inputs and concatenate with subtraction operator"""
-
         return "(" + WorkspaceModel.calculate_unit(self._inputs[0].id) + ") - (" +\
             WorkspaceModel.calculate_unit(self._inputs[1].id) + ")"
