f78be62e8bc6c7d562a6d58bcd462cfc903ab1e5
Fritz Hund <edifice444@gmail.com>
2020-02-01 20:26:54 +0100

    Add documentation for Connection

--- phypigui/python/src/model/item/Connection.py
+++ phypigui/python/src/model/item/Connection.py
@@ -1,12 +1,22 @@
 class Connection:
+    """This class represents a connection between a OutputItem and a InputItem"""
+
     def __init__(self, input_id: int, output_id: int):
+        """Initialising a Connection object
+
+        Args:
+            input_id (int): ID of the InputItem, which is part of the connection
+            output_id (int): ID of the OutputItem, which is part of the connection
+        """
         self.__input: int = input_id
         self.__output: int = output_id
 
     @property
     def input(self) -> int:
+        """ID of the InputItem, which is part of the connection"""
         return self.__input
 
     @property
     def output(self) -> int:
+        """ID of the OutputItem, which is part of the connection"""
         return self.__output
