3c3d15686c6f1a6ae2deb01b8905fbecff1e5b43
Simon Essig <essigsimon@web.de>
2020-02-03 11:43:56 +0100

    added docs to model, view

--- phypigui/python/src/view/View.py
+++ phypigui/python/src/view/View.py
@@ -3,5 +3,10 @@ from typing import NoReturn
 
 
 class View(ABC):
+    """This class represents the view of the model-view design pattern
+
+        It can attach itself to a model to receive updates from it.
+    """
     def update(self) -> NoReturn:
+        """Is called when a model it is subscribed to, broadcasts an update"""
         pass
